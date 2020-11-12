8ddd48681a836a51a599f9d01352d6f1af2cdd24
lixuetao <1651928858@qq.com>
2020-11-12 21:48:59 +0800

    接入 修复bug1

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -56,7 +56,7 @@ class WxController extends Controller
         // $obj=json_decode($obj, true);
         // file_put_contents("aaa.txt",$obj);
         // echo "ok";
-//             $content="未完善";
+             $content="未完善";
         file_put_contents('wx_event.log', $str, FILE_APPEND);
         switch ($obj->MsgType) {
             //  关注
@@ -240,8 +240,8 @@ class WxController extends Controller
 //                    break;
         }
 
-//        echo $this->xiaoxi($obj,$content);
-        return true;
+        echo $this->xiaoxi($obj,$content);
+
 }
 
 
