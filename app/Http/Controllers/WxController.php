b503158b1255dbf30dcd1c36cd9934c7ce6f5393
lixuetao <1651928858@qq.com>
2020-11-12 16:43:14 +0800

    调试

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -31,7 +31,7 @@ class WxController extends Controller
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
 
-        $token ="Li";
+        $token =env('WX_TOKEN');
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
