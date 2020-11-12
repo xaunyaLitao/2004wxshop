e3b367d6bc388bfbf835e43944537db347ac58e9
lixuetao <1651928858@qq.com>
2020-11-12 21:45:48 +0800

    接入 修复bug1

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -56,7 +56,7 @@ class WxController extends Controller
         // $obj=json_decode($obj, true);
         // file_put_contents("aaa.txt",$obj);
         // echo "ok";
-             $content="";
+             $content="未完善";
         file_put_contents('wx_event.log', $str, FILE_APPEND);
         switch ($obj->MsgType) {
             //  关注
